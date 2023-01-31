b49ac8e246fb9cdb2a7d4c1d69a3d1d1a615193f
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-01-31 17:10:45 +0300

    Remove unnecessary imports

--- src/app/showcase/layout/app.module.ts
+++ src/app/showcase/layout/app.module.ts
@@ -5,25 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { ChartModule } from 'primeng/chart';
-import { CheckboxModule } from 'primeng/checkbox';
-import { ChipModule } from 'primeng/chip';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { ListboxModule } from 'primeng/listbox';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { SliderModule } from 'primeng/slider';
-import { TableModule } from 'primeng/table';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { TooltipModule } from 'primeng/tooltip';
-import { TreeModule } from 'primeng/tree';
 import { AppComponent } from './app.component';
 
 import { CarService } from '../service/carservice';
@@ -38,48 +19,18 @@ import { JsonService } from '../service/jsonservice';
 import { PhotoService } from '../service/photoservice';
 import { ProductService } from '../service/productservice';
 
-import { BadgeModule } from 'primeng/badge';
-import { LandingComponent } from '../components/landing/landing.component';
+import { LandingModule } from '../components/landing/landing.module';
 import { AppMainComponent } from './app.main.component';
 import { AppConfigModule } from './config/app.config.module';
 import { AppDemoActionsModule } from './demoactions/app.demoactions.component';
 import { AppFooterComponent } from './footer/app.footer.component';
-import { AppMenuComponent } from './menu/app.menu.component';
+import { AppMenuModule } from './menu/app.menu.module';
 import { AppNewsComponent } from './news/app.news.component';
 import { AppTopbarModule } from './topbar/app.topbar.module';
 
 @NgModule({
-    declarations: [AppComponent, AppNewsComponent, AppMenuComponent, AppFooterComponent, LandingComponent, AppMainComponent],
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        AutoCompleteModule,
-        ButtonModule,
-        BadgeModule,
-        RadioButtonModule,
-        InputSwitchModule,
-        TooltipModule,
-        AppDemoActionsModule,
-        ChartModule,
-        TabMenuModule,
-        SliderModule,
-        CalendarModule,
-        TreeModule,
-        ProgressBarModule,
-        InputNumberModule,
-        ChipModule,
-        SelectButtonModule,
-        TableModule,
-        CheckboxModule,
-        ListboxModule,
-        InputMaskModule,
-        DropdownModule,
-        AppConfigModule,
-        AppTopbarModule
-    ],
+    declarations: [AppComponent, AppNewsComponent, AppFooterComponent, AppMainComponent],
+    imports: [FormsModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, LandingModule, AppDemoActionsModule, AppConfigModule, AppTopbarModule, AppMenuModule],
     providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }, CarService, CountryService, EventService, NodeService, IconService, CustomerService, PhotoService, JsonService, AppConfigService, ProductService],
     bootstrap: [AppComponent]
 })
